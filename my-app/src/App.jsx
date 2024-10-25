6d5d274aa677846aff8de8d68b8c871b3db6a47c
git07tany <anashkina_2007@mail.ru>
2024-10-25 16:29:37 +0300

    vtoroi commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,14 +1,16 @@
 import logo from '../src/pics/logo.svg';
 import search from '../src/pics/search.svg';
-import cart from '../src/pics/cart.svg';
+import cart from '../src/pics/Cart.svg';
+import background from '../src/pics/Background.svg'
 import './App.css';
+
 
 function App() {
   return (
     <>
     <div className='header'>
       <img src= {logo} alt='logo'></img>
-      <p>Organick</p>
+      <p></p>
       <ul className='tab'>
         <li><a href='#'> Home </a></li>
         <li><a href='#'> About </a></li>
@@ -18,13 +20,16 @@ function App() {
         <li><a href='#'> News </a></li>
       </ul>
       <div className='input'>
+        <img src={search} alt=''/>
         <input type='text' />
-        <img src={search} alt=' ' />
       </div>
       <div className='cart'>
         <img src={cart} alt=''/>
         <p> Cart (0) </p>
       </div>
+    </div>
+    <div className='second'>
+      <img src={background} alt=''/>
     </div>
     </>
   );
